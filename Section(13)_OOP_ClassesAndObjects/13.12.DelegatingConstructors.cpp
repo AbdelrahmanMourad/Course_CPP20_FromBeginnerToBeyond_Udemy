c079fec0adaea12a65db5e582eb5cb08d1d5b1ba
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2025-05-20 19:12:17 +0300

    Delega Constructor

--- Section(13)_OOP_ClassesAndObjects/13.12.DelegatingConstructors.cpp
+++ Section(13)_OOP_ClassesAndObjects/13.12.DelegatingConstructors.cpp
@@ -8,37 +8,48 @@ using namespace std;
 class Player
 {
 private:
-   std::string name;
-   int health;
-   int xp;
+    std::string name;
+    int health;
+    int xp;
+
 public:
-// Overloaded Constructors
+    // Overloaded Constructors
     Player();
     Player(std::string name_val);
     Player(std::string name_val, int health_val, int xp_val);
+    void PrintPlayer(void);
 };
 
-Player::Player() 
-    : Player {"None",0,0} {
-        cout << "No-args constructor" << endl;
+Player::Player() : Player{"None", 0, 0}
+{
+    cout << "No-args constructor" << endl;
 }
 
-Player::Player(std::string name_val) 
-   : Player {name_val,0, 0}  {
-           cout << "One-arg constructor" << endl;
-}
-  
-Player::Player(std::string name_val, int health_val, int xp_val) 
-    : name{name_val}, health{health_val}, xp{xp_val} {
-            cout << "Three-args constructor" << endl;
+Player::Player(std::string name_val) : Player{name_val, 0, 0}
+{
+    cout << "One-arg constructor" << endl;
 }
 
-int main() {
-    
-    Player empty;
-    Player frank {"Frank"};
-    Player villain {"Villain", 100, 55};
-    
+Player::Player(std::string name_val, int health_val, int xp_val) /*Delegeation*/ : name{name_val}, health{health_val}, xp{xp_val}
+{
+    cout << "Three-args constructor" << endl;
+}
+void Player::PrintPlayer(void)
+{
+    std::cout << "Name= " << name << " \t,Health= " << health << "\t,xp= " << xp << std::endl;
+}
+
+int main()
+{
+    // Try all the constructors.
+    Player empty;                       // Default Constructor.
+    Player frank{"Frank"};              // Parameterized contstructor-1
+    Player villain{"Villain", 100, 55}; // Paramterized constructor-2
+
+    // Print each.
+    empty.PrintPlayer();
+    frank.PrintPlayer();
+    villain.PrintPlayer();
+
     return 0;
 }
-
--- Section(13)_OOP_ClassesAndObjects/13.13.DelegatingConstructors.cpp
+++ Section(13)_OOP_ClassesAndObjects/13.13.DelegatingConstructors.cpp
@@ -8,37 +8,31 @@ using namespace std;
 class Player
 {
 private:
-   std::string name;
-   int health;
-   int xp;
+    std::string name;
+    int health;
+    int xp;
+
 public:
-// Overloaded Constructors
+    // Overloaded Constructors
+    // Player() = default;
+    // Player() = delete;
     Player();
     Player(std::string name_val);
     Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() 
-    : Player {"None",0,0} {
-        cout << "No-args constructor" << endl;
-}
+Player::Player() : /*Delegate Constructor*/ Player{"None", 0, 0} { cout << "No-args constructor" << endl; }
 
-Player::Player(std::string name_val) 
-   : Player {name_val,0, 0}  {
-           cout << "One-arg constructor" << endl;
-}
-  
-Player::Player(std::string name_val, int health_val, int xp_val) 
-    : name{name_val}, health{health_val}, xp{xp_val} {
-            cout << "Three-args constructor" << endl;
-}
+Player::Player(std::string name_val) : /*Delegate Constructor*/ Player{name_val, 0, 0} { cout << "One-arg constructor" << endl; }
 
-int main() {
-    
+Player::Player(std::string name_val, int health_val, int xp_val) : name{name_val}, health{health_val}, xp{xp_val} { cout << "Three-args constructor" << endl; }
+
+int main()
+{
+
     Player empty;
-    Player frank {"Frank"};
-    Player villain {"Villain", 100, 55};
-    
+    Player frank{"Frank"};
+    Player villain{"Villain", 100, 55};
+
     return 0;
 }
-
